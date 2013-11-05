56696625549e98ba007aaffaedfe06aeb0e3525d
Kazuho Yamaguchi <kyamaguchi@mediweb.jp>
2013-11-05 11:39:04 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GhprbTest::Application.config.secret_key_base = '6d79cd449f98df3ff47c2cc7c3bf8f16ae4b448e07bca17860b8bc149427847e3fb5bc3287f409cd2ed336fe646bcfd871cb3bf05e0ff2250062587f62e1885f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GhprbTest::Application.config.session_store :cookie_store, key: '_ghprb_test_session'
